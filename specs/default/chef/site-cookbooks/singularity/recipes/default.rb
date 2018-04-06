becc79924d7918a1b1ca68722a9b61a918c9df9b
Ben Watrous <ben.watrous@microsoft.com>
2018-04-05 17:49:22 -0700

    First attempt to download package, if not found, then build RPM or DEB from source

--- specs/default/chef/site-cookbooks/singularity/recipes/default.rb
+++ specs/default/chef/site-cookbooks/singularity/recipes/default.rb
@@ -4,6 +4,7 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+include_recipe 'build-essential'
 
 VERSION = node['singularity']['version']
 
@@ -13,49 +14,107 @@ platform_version = node[:platform_version]
 
 package "squashfs-tools"
 
+directory node[:jetpack][:downloads] do
+  recursive true
+end
+
 # TODO : First check if pre-built packages exist
 #      : Need to figure out rpm and deb package naming
 
-# if myplatform == "centos"
-#   jetpack_download "singularity-#{VERSION}-0.1.el7.centos.x86_64.rpm" do
-#     project "singularity"
-#     not_if { ::File.exist?("#{node[:jetpack][:downloads]}/singularity-#{VERSION}-0.1.el7.centos.x86_64.rpm") }
-#   end
-#
-#   yum_package "singularity-#{VERSION}-0.1.el7.centos.x86_64.rpm" do
-#     source "#{node[:jetpack][:downloads]}/singularity-#{VERSION}-0.1.el7.centos.x86_64.rpm"
-#     action :install
-#   end
-# else
-#   jetpack_download "singularity-container_#{VERSION}-1_amd64.deb" do
-#     project "singularity"
-#     not_if { ::File.exist?("#{node[:jetpack][:downloads]}/singularity-container_#{VERSION}-1_amd64.deb") }
-#   end
-#
-#   dpkg_package "singularity-container_#{VERSION}-1_amd64.deb" do
-#     source "#{node[:jetpack][:downloads]}/singularity-container_#{VERSION}-1_amd64.deb"
-#     action :install
-#   end
-# end
+if myplatform == "centos"
+  singularity_rpm="singularity-#{VERSION}-1.el7.centos.x86_64.rpm"
+  singularity_rpm_path="#{node[:jetpack][:downloads]}/#{singularity_rpm}"
 
-# ELSE build from source
-include_recipe 'build-essential'
+  # Check if the RPM is available
+#   jetpack_download singularity_rpm do
+#     project "singularity"
+#     not_if { ::File.exist?(singularity_rpm_path) }
+#   end
+  execute "#{node['cyclecloud']['jetpack']['executable']} download #{singularity_rpm} #{singularity_rpm_path}  --project singularity" do
+    returns [0, 1]
+    not_if { ::File.exists?(singularity_rpm_path) }
+  end
 
-jetpack_download "singularity-#{VERSION}.tar.gz" do
-  project "singularity"
-  not_if { ::File.exist?("#{node[:jetpack][:downloads]}/singularity-#{VERSION}.tar.gz") }
+  # ELSE build from source
+  jetpack_download "singularity-#{VERSION}.tar.gz" do
+    project "singularity"
+    not_if { ::File.exists?("#{node[:jetpack][:downloads]}/singularity-#{VERSION}.tar.gz") }
+    not_if { ::File.exists?(singularity_rpm_path) }
+  end
+
+  %w"automake rpm-build".each do |pkg|
+    package pkg do
+      not_if ::File.exists?(singularity_rpm_path)
+    end
+  end  
+
+  bash 'make singularity rpm' do
+    cwd "/tmp"
+    code <<-EOH
+         set -e
+         tar xzvf #{node[:jetpack][:downloads]}/singularity-#{VERSION}.tar.gz
+         cd singularity-#{VERSION}
+         ./autogen.sh
+         ./configure
+         make dist
+         rpmbuild -ta singularity-*.tar.gz
+         mv ~/rpmbuild/RPMS/*/#{singularity_rpm} #{singularity_rpm_path}
+         EOH
+    not_if { ::File.exists?( singularity_rpm_path ) }
+  end
+
+  yum_package "#{singularity_rpm}" do
+    source "#{singularity_rpm_path}"
+    action :install
+  end
+else
+  singularity_deb="singularity-container_#{VERSION}-1_amd64.deb"
+  singularity_deb_path="#{node[:jetpack][:downloads]}/#{singularity_deb}"
+  
+  # Check if the DEB is available
+#   jetpack_download singularity_deb do
+#     project "singularity"
+#     not_if { ::File.exist?(singularity_deb_path) }
+#   end
+  execute "#{node['cyclecloud']['jetpack']['executable']} download #{singularity_deb} #{singularity_deb_path}  --project singularity" do
+    returns [0, 1]
+    not_if { ::File.exists?(singularity_deb_path) }
+  end
+  
+  # ELSE build from source
+  jetpack_download "singularity-#{VERSION}.tar.gz" do
+    project "singularity"
+    not_if { ::File.exists?("#{node[:jetpack][:downloads]}/singularity-#{VERSION}.tar.gz") }
+    not_if { ::File.exists?(singularity_deb_path) }
+  end
+
+
+  %w"debhelper dh-autoreconf help2man".each do |pkg|
+    package pkg do
+      not_if ::File.exists?(singularity_deb_path)
+    end
+  end
+  
+  bash 'make singularity deb' do
+    cwd "/tmp"
+    code <<-EOH
+         set -e
+         tar xzvf #{node[:jetpack][:downloads]}/singularity-#{VERSION}.tar.gz
+         cd singularity-#{VERSION}
+         # Disable tests
+         echo "echo SKIPPING TESTS THEYRE BROKEN" > ./test.sh
+         dpkg-buildpackage -b -us -uc
+         mv ../singularity-container_*amd64.deb #{singularity_deb_path}
+         EOH
+    not_if { ::File.exists?( singularity_deb_path ) }
+  end
+  
+  dpkg_package singularity_deb do
+    source singularity_deb_path
+    action :install
+  end
 end
 
 
-bash 'make singularity' do
-  cwd "/tmp"
-  code <<-EOH
-       tar xzvf #{node[:jetpack][:downloads]}/singularity-#{VERSION}.tar.gz
-       cd singularity-#{VERSION}
-       ./configure --prefix=/usr/local
-       make
-       make install
-       EOH
-  not_if { ::File.exist?("/usr/local/libexec/singularity") }
-end
+
 
